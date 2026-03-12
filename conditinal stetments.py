8a25214eeed392a3f64265b5753d47b92aee76e8
Aradhy Singh chouhan <aradhysinghchouhan4@gmail.com>
2026-03-12 15:08:16 +0530

    Add files via upload

---
+++ conditinal stetments.py
@@ -0,0 +1,18 @@
+light = "green"
+
+if(light =="red"):
+    print("stop")
+elif(light =="yellow"):
+    print("wait")
+else:
+    print("go")
+
+num = 6 
+if(num <2):
+    print("greater than two")
+if(num <4):
+    print("smaller than 2")
+elif(num == 5):
+    print("number found")
+else:
+    print("number not found")
